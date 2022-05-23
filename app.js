f52297a8b356f318d46cc56b3300d826bb7243e4
godchurch <106052556+godchurchart@users.noreply.github.com>
2022-05-23 11:43:29 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-//const firstname = 'Babayan'
-//const lastname = prompt('write your name')
-//alert(firstname + '  ' + lastname );
+const firstname = 'Good Day'
+           const lastname = prompt('Your Name?')
+alert(firstname + '  ' + lastname );
 
-const nowyear = 2022
-const birthyear = 2005
-const age = nowyear - birthyear
+//const nowyear = 2022
+  //const birthyear = 2005
+   //const age = nowyear - birthyear
 
-console.log(age);
+//console.log(age);
 
-const courseStatus = 'pending'
-if (courseStatus === 'ready') {
- console.log('kurs uje gatov n ego mojno proxodit')
-} else if (courseStatus === 'pending') {
- console.log('kurs poka naxoditsa v procese razrabotki')
-}
+//const courseStatus = 'pending'
+  //if (courseStatus === 'ready') {
+    //console.log('kurs uje gatov n ego mojno proxodit')
+    //} else if (courseStatus === 'pending') {
+//console.log('kurs poka naxoditsa v procese razrabotki')}
 
-const isReady = true
-if(isReady === true) {
+//const isReady = true
+//if(isReady === true) {
 
- console.log('vso gatovo')
-}
+// console.log('vso gatovo')
+//}
 
-function calculateAge(year) {
- return 2022 - year
-} 
+//function calculateAge(year) {
+ //return 2022 - year
+//} 
 
-console.log(calculateAge(1937))
+//console.log(calculateAge(1937))
 
